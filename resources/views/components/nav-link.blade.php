e17f6282b934d254f16dc73792a7898844d8f99d
millahh <millahmillah37@gmail.com>
2024-05-18 22:08:41 +0700

    sidebar fixed

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'px-1 text-lg font-bold leading-5 text-[#FEFEFE] hover:shadow-md focus:shadow-md active:shadow-md'
-            : 'px-1 border-b-2 border-transparent text-sm font-medium leading-5';
+            ? 'px-1 text-lg font-bold rounded-md border-transparent text-[#77AFB7] leading-5 shadow-xl bg-[#FDFDFD]'
+            : 'px-1 text-lg font-bold leading-5 shadow-none bg-[#77AFB7] text-[#FDFDFD]';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
