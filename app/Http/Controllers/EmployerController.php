b509bab3b1a1b30824e39062f2e4273c2d8c8149
millahh <millahmillah37@gmail.com>
2024-06-20 23:05:59 +0700

    solved minor bug3

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Support\MessageBag;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Employer;
 use App\Models\Employee;
 use App\Models\User;
@@ -47,7 +51,7 @@ class EmployerController extends Controller
         return view('tracker.tracker-employer.finished',['tasks'=> $tasks, 'tasks_id'=>$tasks_id]);
     }
 
-    public function store(Request $request){
+    public function store(Request $request): RedirectResponse {
         $employer_data = $request->validate([
             'task_name' => ['required'],
             'task_desc' => ['required'],
@@ -55,7 +59,14 @@ class EmployerController extends Controller
             'task_due' => ['required'],
             'task_assignments' => ['required'],
         ]);
+
         $employer_data['user_id'] = $request->user()->id;
+
+        $employer_data2['task_checkpoints']=[];
+        foreach($employer_data['task_checkpoints'] as $checkpoint){
+            if (!is_null($checkpoint)) array_push($employer_data2['task_checkpoints'],$checkpoint);
+        }
+        $employer_data['task_checkpoints']=$employer_data2['task_checkpoints'];
         $employer = Employer::create($employer_data);
 
         foreach ($employee_loop = Employee::all() as $employee_loop) {
@@ -68,7 +79,6 @@ class EmployerController extends Controller
             }
             $employee_loop->update($employee_data);
         }
-
         return redirect()->route('tasks', $employer)->with('success', 'Task Created Successfully');
     }
 
@@ -89,12 +99,16 @@ class EmployerController extends Controller
 
         foreach ($employee_loop = Employee::all() as $employee_loop) {
             $employee_data['tasks_id']=[];
+            if(!empty($employee_loop->tasks_id)) $employee_data['tasks_id'] = $employee_loop->tasks_id;
             foreach($employer->task_assignments as $keys){
                 if ((substr($keys, 2))==="true" && $employee_loop->user_id == (int)$keys[0]) {
-                    $employee_data['tasks_id'] = (array)$employee_loop->tasks_id; 
                     array_push($employee_data['tasks_id'] , $employer->id); 
+                }elseif ((substr($keys, 2))==="false" && $employee_loop->user_id == (int)$keys[0]) {
+                    $employee_data['tasks_id'] = array_diff( $employee_data['tasks_id'], [$employer->id] );
+                    $employee_data['tasks_id'] = array_values($employee_data['tasks_id']);
                 }
             }
+            $employee_data['tasks_id'] = array_unique($employee_data['tasks_id']);
             $employee_loop->update($employee_data);
         }
         return redirect()->route('tasks', $employer)->with('success', 'Task Edited Successfully');
